a7aa1c2818e86c375d02f43f6383acf164da0c11
Siew Tian Cheng <siewtiancheng0224@e.newera.edu.my>
2021-12-03 01:45:12 +0800

    Initial commit

---
+++ app/src/main/java/com/example/classicmusicplayer/MusicAdapter.java
@@ -0,0 +1,88 @@
+package com.example.classicmusicplayer;
+
+import android.content.Context;
+import android.content.Intent;
+import android.media.MediaMetadataRetriever;
+import android.net.Uri;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+
+public class MusicAdapter extends RecyclerView.Adapter<MusicAdapter.MyViewHolder> {
+
+    private Context mContext;
+    private ArrayList<MusicFile> mFiles;
+
+    MusicAdapter(Context mContext, ArrayList<MusicFile> mFiles)
+    {
+        this.mFiles = mFiles;
+        this.mContext = mContext;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(mContext).inflate(R.layout.music_items, parent, false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MusicAdapter.MyViewHolder holder, int position) {
+        holder.file_name.setText(mFiles.get(position).getTitle());
+        byte[] image = getAlbumArt(mFiles.get(position).getPath());
+        if (image != null)
+        {
+            Glide.with(mContext).asBitmap()
+                    .load(image)
+                    .into(holder.album_art);
+        }
+        else
+        {
+            Glide.with(mContext)
+                    .load(R.drawable.noalbumart)
+                    .into(holder.album_art);
+        }
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(mContext, PlayerActivity.class);
+                mContext.startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mFiles.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder
+    {
+        TextView file_name;
+        ImageView album_art;
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            file_name = itemView.findViewById(R.id.music_file_name);
+            album_art = itemView.findViewById(R.id.music_img);
+        }
+    }
+
+    private byte[] getAlbumArt(String uri)
+    {
+        MediaMetadataRetriever retriever = new MediaMetadataRetriever();
+        retriever.setDataSource(uri);
+        byte[] art = retriever.getEmbeddedPicture();
+        retriever.release();
+        return art;
+    }
+}
---
+++ app/src/main/java/com/example/classicmusicplayer/PlayerActivity.java
@@ -0,0 +1,40 @@
+package com.example.classicmusicplayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class PlayerActivity extends AppCompatActivity {
+
+    TextView song_name, artist_name, duration_played, duration_total;
+    ImageView cover_art, nextBtn, prevBtn, backBtn, shuffleBtn, repeatBtn;
+    FloatingActionButton playPauseBtn;
+    SeekBar seekBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_player);
+        initViews();
+    }
+
+    private void initViews() {
+        song_name = findViewById(R.id.song_name);
+        artist_name = findViewById(R.id.song_artist);
+        duration_played = findViewById(R.id.durationPlayed);
+        duration_total = findViewById(R.id.durationTotal);
+        cover_art = findViewById(R.id.cover_art);
+        nextBtn = findViewById(R.id.id_next);
+        prevBtn = findViewById(R.id.id_prev);
+        backBtn = findViewById(R.id.back_btn);
+        shuffleBtn = findViewById(R.id.id_shuffle);
+        repeatBtn = findViewById(R.id.id_repeat);
+        playPauseBtn = findViewById(R.id.play_pause);
+        seekBar = findViewById(R.id.seekBar);
+    }
+}
